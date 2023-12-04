9e7d24bc8419c13fbb97cdaf33c7a939186d18c0
MishiMads <mnj-777@hotmail.com>
2023-12-04 10:49:31 +0100

    Kode mere SFW

--- Assets/ButtonClickHandler.cs
+++ Assets/ButtonClickHandler.cs
@@ -3,18 +3,7 @@ using UnityEngine;
 public class ButtonClickHandler : MonoBehaviour
 {
     public UnitySocketClient socketClient;
-
-    public void OnButtonClick()
-    {
-        // Replace "YourMessage" with the actual message you want to send to the Python server
-        socketClient.SendData("Poosay");
-    }
     
-    public void OnButtonClick1()
-        {
-            // Replace "YourMessage" with the actual message you want to send to the Python server
-            socketClient.SendData("Nej Mads");
-        }
     public void OnRunRealsenseScriptButtonClick()
     {
         socketClient.SendData("123");
